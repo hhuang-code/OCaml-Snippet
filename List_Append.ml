050ffdf1e23fb21a573a01dbc465e11fa4dea735
huanghao-code <huanghao_bupt@sina.com>
2017-10-11 16:37:12 -0400

    Update List_Append.ml

--- List_Append.ml
+++ List_Append.ml
@@ -4,5 +4,11 @@ open Printf
 let rec append l1 l2 =
     if l1 = [] then l2
     else hd l1 :: append (tl l1) l2;;
+    
+(* A better version: *)
+let rec append l1 l2 =
+    match l1 with
+    |[] -> l2
+    |h::t -> h :: append t l2;;
 
 let () = List.iter (printf "%d ") (append [1; 2] [3; 4]);;
