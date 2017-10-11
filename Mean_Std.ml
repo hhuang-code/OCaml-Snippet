5f49b70acfe49f4519a99cf4a3dccda968a0d6a5
huanghao-code <huanghao_bupt@sina.com>
2017-10-11 17:00:22 -0400

    Create Mean_Std.ml

---
+++ Mean_Std.ml
@@ -0,0 +1,16 @@
+open Printf
+
+let stats l = 
+    let rec helper rest n sum sum_squares = 
+        match rest with
+        | [] -> let nf = float_of_int n in
+                (sum /. nf, sqrt(sum_squares /. nf))
+        | h :: t -> helper t (n + 1) (sum +. h) (sum_squares +. (h *. h)) in
+    helper l 0 0.0 0.0;;
+    
+let (mean, std) = stats [1.; 2.; 3.; 4.; 5.];;
+
+let () = 
+    print_float mean;
+    print_newline ();
+    print_float std;;
