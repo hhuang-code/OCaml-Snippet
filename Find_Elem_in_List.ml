c36120dad4f6ae80ffd411c338758b2c618ebccc
huanghao-code <huanghao_bupt@sina.com>
2017-10-11 16:21:11 -0400

    Update Find_Elem_in_List.ml

--- Find_Elem_in_List.ml
+++ Find_Elem_in_List.ml
@@ -6,3 +6,17 @@ let rec member x list =
 let () = 
     if (member 1 [1; 2; 3; 4]) = true then printf "yes"
     else printf "no";;
+    
+(*---or---*)
+
+let rec member key list =
+    match list with
+    |[] -> false
+    |v :: rest when v = key -> true
+    |head :: rest -> member key rest;;
+    
+let squares = [1; 2; 3; 4; 5; 6];;
+
+let () = 
+    if (member 9 squares) then printf("true")
+    else printf("false");;
